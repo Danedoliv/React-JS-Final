360a11447e49a5e349e2907fbd2313612e981b3a
Daniel Olivier <danielolivier8@hotmail.com>
2022-01-31 00:21:51 -0300

    final

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ReactRoutes from './ReactRoutes/ReactRoutes.jsx';
+
+function App() {
+  return (
+    <div>
+      <ReactRoutes/>
+   
+    </div>
+  );
+}
+
+export default App;
